4ff49f1e94aedb2a75f98f51e6e3bfb99cb0a80b
Fareed Dudhia <fareeddudhia@googlemail.com>
2013-07-25 19:57:12 +0000

    Initial

---
+++ prospapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from prospapp.models import *
+
+admin.site.register(BaseImage)
+admin.site.register(TestImage)
+admin.site.register(Test)
+admin.site.register(TestInstance)
+admin.site.register(Account)
