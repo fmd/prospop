06f34d42909837cc483cf1d3161ce693698e51ca
Fareed Dudhia <fareeddudhia@googlemail.com>
2013-07-26 20:05:50 +0000

    removed all files for new gitignore

--- prospapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from prospapp.models import *
-
-admin.site.register(BaseImage)
-admin.site.register(TestImage)
-admin.site.register(Test)
-admin.site.register(TestInstance)
-admin.site.register(Account)
--- prospop/views.py
+++
@@ -1,9 +0,0 @@
-from django.template import Template, Context
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def show_404(request):
-    return render(request, '404.html', {})
-
-def show_500(request):
-    return render(request, '500.html', {})
