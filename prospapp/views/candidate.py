a485c852745f90941ca42f804d1f4f73be128393
Fareed Dudhia <fareeddudhia@googlemail.com>
2013-07-30 20:06:26 +0000

    added candidate scripts to view package

---
+++ prospapp/views/candidate.py
@@ -0,0 +1,92 @@
+#########################################
+##### ----- Candidate Section ----- #####
+#########################################
+
+### Imports ###
+
+from helpers import *
+
+### Candidate Account View ###
+
+def candidate(request):
+    auth = authenticate_type(request.user,"CANDIDATE")
+    if not auth:
+
+        # TODO
+        # Error message: You must be logged in to continue.
+
+        return redirect("/candidate/login/")
+    context = {
+        'authenticated' : auth,
+        'email'         : request.user.email,
+        'instances'     : request.user.instances.all(),
+    }  
+    return render(request, 'candidate/home.html', context)
+
+### Candidate Login View ###
+
+def view_candidate_login(request):
+    response = ensure_unauthorized(request)
+    if response:
+
+        # TODO
+        # Error message: You must be logged in to continue.
+
+        return response
+    
+    auth = authenticate_type(request.user,"CANDIDATE")
+    context = {
+        'authenticated' : auth,
+    }
+    context.update(csrf(request))
+    return render(request, 'candidate/login.html',context)
+
+### Candidate Login Action ###
+
+def action_candidate_login(request, onsuccess='/candidate/', onfail='/candidate/login/'):
+    user = authenticate(username=request.POST['email'], password=request.POST['password'])
+    auth = authenticate_type(user,"CANDIDATE")
+    if not auth:
+
+        # TODO
+        # Error message: Invalid username or password,
+
+        return redirect("/candidate/login/")
+
+    if user is not None:
+        login(request, user)
+        return redirect(onsuccess)
+    else:
+
+        # TODO
+        # Error message: This is not an active user.
+
+        return redirect(onfail) 
+
+### Candidate Signup View ###
+
+def view_candidate_signup(request):
+    auth = authenticate_type(request.user,"CANDIDATE")
+    context = {
+        'authenticated' : auth,
+    }
+    context.update(csrf(request))
+    return render(request, 'candidate/signup.html', context)
+
+### Candidate Signup Action ###
+
+def action_candidate_signup(request, onsuccess='/candidate/login/', onfail='/candidate/signup/'):
+    post = request.POST
+
+    # TODO
+    # Validate candidate signup
+
+    if not user_exists(post['email']): 
+        user = create_user(username=post['email'], email=post['email'], password=post['password'], type="CANDIDATE")
+        return redirect(onsuccess)
+    else:
+
+        # TODO
+        # Error message: A user with this email address already exists.
+
+        return redirect(onfail)
