0ea6cfec5a98f882f140d0d1376314585fde0753
Fareed Dudhia <fareeddudhia@googlemail.com>
2013-09-03 09:07:06 +0000

    Added authorization models for Tests

---
+++ prospapp/migrations/0006_auto__add_testauth.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'TestAuth'
+        db.create_table(u'prospapp_testauth', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')(blank=True)),
+            ('updated_at', self.gf('django.db.models.fields.DateTimeField')(blank=True)),
+            ('deleted_at', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('test', self.gf('django.db.models.fields.related.ForeignKey')(related_name='authorizations', to=orm['prospapp.Test'])),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(related_name='authorized_tests', blank=True, to=orm['prospapp.Account'])),
+            ('email', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('key', self.gf('django.db.models.fields.CharField')(max_length=32, blank=True)),
+        ))
+        db.send_create_signal(u'prospapp', ['TestAuth'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'TestAuth'
+        db.delete_table(u'prospapp_testauth')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'prospapp.account': {
+            'Meta': {'object_name': 'Account'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'type': ('django.db.models.fields.CharField', [], {'default': "'CANDIDATE'", 'max_length': '31'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'prospapp.baseimage': {
+            'Meta': {'object_name': 'BaseImage'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'deleted_at': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'identifier': ('django.db.models.fields.CharField', [], {'max_length': '127', 'blank': 'True'}),
+            'label': ('django.db.models.fields.CharField', [], {'max_length': '31'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '31'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'images'", 'null': 'True', 'to': u"orm['prospapp.BaseImage']"}),
+            'tag': ('django.db.models.fields.CharField', [], {'max_length': '31', 'blank': 'True'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'})
+        },
+        u'prospapp.test': {
+            'Meta': {'object_name': 'Test'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'deleted_at': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'tests'", 'to': u"orm['prospapp.TestImage']"}),
+            'instructions': ('django.db.models.fields.TextField', [], {'default': "''"}),
+            'is_public': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'label': ('django.db.models.fields.CharField', [], {'max_length': '31'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'tests'", 'to': u"orm['prospapp.Account']"}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'})
+        },
+        u'prospapp.testauth': {
+            'Meta': {'object_name': 'TestAuth'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'deleted_at': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'key': ('django.db.models.fields.CharField', [], {'max_length': '32', 'blank': 'True'}),
+            'test': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'authorizations'", 'to': u"orm['prospapp.Test']"}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'authorized_tests'", 'blank': 'True', 'to': u"orm['prospapp.Account']"})
+        },
+        u'prospapp.testimage': {
+            'Meta': {'object_name': 'TestImage', '_ormbases': [u'prospapp.BaseImage']},
+            u'baseimage_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['prospapp.BaseImage']", 'unique': 'True', 'primary_key': 'True'}),
+            'instructions': ('django.db.models.fields.TextField', [], {'default': "''"}),
+            'is_public': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'images'", 'to': u"orm['prospapp.Account']"})
+        },
+        u'prospapp.testinstance': {
+            'Meta': {'object_name': 'TestInstance'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'deleted_at': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'instances'", 'to': u"orm['prospapp.Account']"}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'PENDING'", 'max_length': '31'}),
+            'test': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'instances'", 'to': u"orm['prospapp.Test']"}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['prospapp']
--- prospapp/models.py
+++ prospapp/models.py
@@ -4,8 +4,7 @@ from django.db.models.signals import post_save
 from django.utils import timezone
 from django.utils.timezone import utc
 
-import pytz
-import datetime
+import pytz, datetime, base64, hashlib
 
 #################################
 ##### ----- Constants ----- #####
@@ -30,17 +29,11 @@ TESTINSTANCE_STATUSES = (
 ##### ----- Models ----- #####
 ##############################
 
-############################
 ### User class extension ###
-############################
-
 class Account(AbstractUser):
     type = models.CharField(choices=ACCOUNT_TYPES, max_length=31, default="CANDIDATE")
 
-########################################
 ### Base model class with CRUD data. ###
-########################################
-
 class BaseModel(models.Model):
     created_at = models.DateTimeField(blank=True)
     updated_at = models.DateTimeField(blank=True)
@@ -56,10 +49,7 @@ class BaseModel(models.Model):
         self.updated_at = time
         super(BaseModel, self).save(*args, **kwargs)
 
-##################################################
 ### Database representation of a Docker image. ###
-##################################################
-
 class BaseImage(BaseModel):
     label      = models.CharField(max_length=31)                                                           # (Front-facing) label for the image.
     name       = models.CharField(max_length=31)                                                           # The name of the actual Docker image.
@@ -70,19 +60,13 @@ class BaseImage(BaseModel):
     def __unicode__(self):
         return self.label + " (" + self.name + ":" + self.tag + ")"
 
-###########################################################
 ### Database representation of a Clonable Docker "Test" ###
-###########################################################
-
 class TestImage(BaseImage):
     owner            = models.ForeignKey(Account, related_name="images", parent_link=True)
     is_public        = models.BooleanField(default=True)
     instructions     = models.TextField(default="")
 
-##########################################################
 ### Database representation of an open Prospop "Test". ###
-##########################################################
-
 class Test(BaseModel):
     label        = models.CharField(max_length=31)
     image        = models.ForeignKey(TestImage, related_name="tests", parent_link=True) 
@@ -97,12 +81,23 @@ def clone_instructions(sender, instance, created, **kwargs):
 
 post_save.connect(clone_instructions, sender=Test) 
 
-####################################################################
 ### Database representation of a Docker container running a Test ###
-####################################################################
-
 class TestInstance(BaseModel):
     test   = models.ForeignKey(Test, related_name="instances", parent_link=True)
     owner  = models.ForeignKey(Account, related_name="instances", parent_link=True)
     status = models.CharField(choices=TESTINSTANCE_STATUSES,max_length=31,default="PENDING") 
 
+### A Permissions object between Tests and Accounts ###
+class TestAuth(BaseModel):
+    test  = models.ForeignKey(Test, related_name="authorizations")
+    user  = models.ForeignKey(Account, related_name="authorized_tests", blank=True, null=True)
+    email = models.CharField(max_length=255, blank=True)
+    key   = models.CharField(max_length=32,  blank=True)
+
+def get_key(sender, instance, created, **kwargs):
+    if created:
+        hasher = hashlib.sha1(str(instance.created_at) + str(instance.id))
+        instance.key = base64.urlsafe_b64encode(hasher.digest()[0:31])
+        instance.save()
+
+post_save.connect(get_key, sender=TestAuth)
